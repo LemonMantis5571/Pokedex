de98a314be8134e3965667b3cc12ff122713b4d3
Leonel Guerrero <85099589+LemonMantis5571@users.noreply.github.com>
2022-06-19 12:51:41 -0600

    pokemones añadidos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,36 @@
 // Grab elements
+const pokedex = document.getElementById("pokedex");
+console.log(pokedex);
 
-//Nav styles on scroll
 
-// Open menu & search pop-up
+const promises = [];
+const fetchPokemon = ()  => {
+    for (let i = 1 ; i < 10 ; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then(response => response.json()));
+    }
 
-// Open/Close search form popup
+        Promise.all(promises).then(results => { 
+            const pokemon = results.map(data => ({
+                name: data.name,
+                id: data.id,
+                image: data.sprites['front_default'],
+                types: data.types.map(type => type.type.name).join(', ')
+            }));
+            MostrarPokemon(pokemon);
+        });
+        
+};
 
-// -- Close the search form popup on ESC keypress
+const MostrarPokemon = (pokemon) => {
+    console.log(pokemon);
+    const PokemonHTMLString = pokemon.map(digimon => `
+    <li class="card">
+    <img class="card-image" src="${digimon.image}"/>
+    <h3 class="Pokemon-info">${digimon.id}. ${digimon.name.toUpperCase()}</h3>
+    <p class="Pokemon-Description">Type: ${digimon.types}</p>
+    </li>`).join('');
+    pokedex.innerHTML = PokemonHTMLString;
+}
 
-// Switch theme/add to local storage
-
-// Swiper
+fetchPokemon();
