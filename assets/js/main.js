7f46f21b13bd711c489c0ba121b305525070af94
Leonel Guerrero <85099589+LemonMantis5571@users.noreply.github.com>
2022-06-20 17:25:51 -0600

    YA MUESTRA EL TURCASO DE POKEMONES

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,80 @@
 // Grab elements
 const pokedex = document.getElementById("pokedex");
+const pokemonContainer = document.querySelector(".pokedex");
+const previous = document.querySelector("#previous");
+const next = document.querySelector("#next");
 console.log(pokedex);
+console.log(previous);
+console.log(next);
+
+let limit = 8;
+let offset = 1;
+
+
+previous.addEventListener("click", () => {
+    if (offset != 1) {
+      offset -= 9;
+      removeChildNodes(pokemonContainer);
+      fetchPokemons(offset, limit);
+    }
+});
+  
+  next.addEventListener("click", () => {
+    offset += 9;
+    removeChildNodes(pokemonContainer);
+    fetchPokemons(offset, limit);
+});
 
 
 const promises = [];
-const fetchPokemon = ()  => {
-    for (let i = 1 ; i < 10 ; i++) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(url).then(response => response.json()));
-    }
+function fetchPokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+      .then((res) => res.json())
+      .then((data) => {
+        const pokemon = {
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            types: data.types.map(type => type.type.name).join(', ')
 
-        Promise.all(promises).then(results => { 
-            const pokemon = results.map(data => ({
-                name: data.name,
-                id: data.id,
-                image: data.sprites['front_default'],
-                types: data.types.map(type => type.type.name).join(', ')
-            }));
-            MostrarPokemon(pokemon);
-        });
-        
+        }
+        MostrarPokemon(pokemon);
+      });
 };
 
-const MostrarPokemon = (pokemon) => {
-    console.log(pokemon);
-    const PokemonHTMLString = pokemon.map(digimon => `
-    <li class="card">
-    <img class="card-image" src="${digimon.image}"/>
-    <h3 class="Pokemon-info">${digimon.id}. ${digimon.name.toUpperCase()}</h3>
-    <p class="Pokemon-Description">Type: ${digimon.types}</p>
-    </li>`).join('');
-    pokedex.innerHTML = PokemonHTMLString;
+function fetchPokemons(offset, limit) {
+    for (let i = offset; i <= offset + limit; i++) {
+      fetchPokemon(i);
+    }
 }
 
-fetchPokemon();
+
+fetchPokemons(offset, limit);
+
+function MostrarPokemon(pokemon){
+    const card = document.createElement('li');
+    card.classList.add('card');
+    const cardImage = document.createElement('img');
+    cardImage.classList.add('card-image');
+    cardImage.src = pokemon.image;
+    const cardInfo = document.createElement('h3');
+    cardInfo.classList.add('Pokemon-info');
+    cardInfo.innerHTML = `${pokemon.id}. ${pokemon.name.toUpperCase()}`;
+    const cardDescription = document.createElement('p');
+    cardDescription.classList.add('Pokemon-Description');
+    cardDescription.innerHTML = `Type: ${pokemon.types}`;
+    card.appendChild(cardImage);
+    card.appendChild(cardInfo);
+    card.appendChild(cardDescription);
+    pokemonContainer.appendChild(card);
+}
+
+
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+}
+/* Swiper */
+
+
